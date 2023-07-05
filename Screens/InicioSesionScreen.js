fbe0f9cd82bd08c9163195c02d959cc31b045973
RomiBettiol <86077944+RomiBettiol@users.noreply.github.com>
2023-07-05 19:06:54 -0300

    Comit 5/07

--- Screens/InicioSesionScreen.js
+++ Screens/InicioSesionScreen.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import { Image } from 'react-native-elements';
-import { ImageBackground } from 'react-native';
+import { ImageBackground} from 'react-native';
+import Registro from '../componentes/Registro';
 
 export function InicioSesionScreen({navigation}){
   return(
@@ -12,24 +13,8 @@ export function InicioSesionScreen({navigation}){
         <View style={styles.contenedor1}>
             <Text style={styles.titulo}>¡HOLA!</Text>
             <Text style={styles.subtitulo}>Bienvenido a BUDDY!</Text>
-            <View style={styles.contenedor2}>
-                <Text>Usuario</Text>
-                <Text>Contraseña</Text>
-                <TouchableOpacity style={[styles.boton,styles.shadowProp]}>
-                    <Text>Ingresar</Text>
-                </TouchableOpacity>
-                <Text>¿Olvidaste tu contraseña?</Text>
-                <Text>Ingresa con</Text>
-                <Image
-                    source={require('../Imagenes/facebook.png')}
-                    style={styles.imagen}
-                />
-                <Image
-                source={require('../Imagenes/social.png')}
-                style={styles.imagen}
-                />
-            </View>
         </View>
+        <Registro />
     </ImageBackground>
   );
 }
@@ -42,48 +27,16 @@ const styles = StyleSheet.create({
   },
 
   titulo: {
-    fontSize: 35,
+    fontSize: 50,
     marginBottom: '5%',
   },
 
   subtitulo: {
-    fontSize: 25,
+    fontSize: 30,
   },
 
   contenedor1: {
     marginTop: '50%',
     alignItems: 'center',
   },
-
-  contenedor2: {
-    marginTop: '10%',
-    backgroundColor: '#DDC4B8',
-    alignItems:'center',
-    justifyContent: 'flex-start',
-    paddingTop: '17%',
-    borderTopLeftRadius: 140,
-    width: '80%',
-    height: '100%',
-    marginLeft: '20%',
-  },
-
-  boton:{
-    borderRadius: 10,
-    marginTop: '10%',
-    backgroundColor: '#FFB984',
-    width: '50%',
-    height: '5%',
-    paddingLeft: '5%',
-    paddingRight: '5%',
-    justifyContent: 'center',
-    marginBottom: '10%',
-    alignItems: 'center',
-    elevation: 10,
-  },
-
-  imagen: {
-    width: 40, 
-    height: 40,
-  },
-
 });
