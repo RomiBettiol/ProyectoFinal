f22788e7b0d5c82fe5e68fbd85d8a42ca8928dbc
RomiBettiol <86077944+RomiBettiol@users.noreply.github.com>
2023-08-02 23:16:46 -0300

    02/08

--- Screens/BusquedaScreen.js
+++ Screens/BusquedaScreen.js
@@ -9,32 +9,6 @@ import axios from 'axios';
 
 export default function BusquedaScreen() {
   const navigation = useNavigation();
-  const [publicaciones, setPublicaciones] = useState([]); // Estado para almacenar las publicaciones
-
-  const getPublicaciones = () => {
-    // Hacer la petición GET al backend usando axios
-    axios
-      .get('http://10.0.2.2:4000/publication/publications?modelType=adoption', {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-      .then((response) => {
-        console.log('Respuesta exitosa:', response.data);
-
-        // Extraer los datos necesarios del backend y guardarlos en el estado 'publicaciones'
-        const data = response.data.data;
-        if (data && Array.isArray(data)) {
-          setPublicaciones(data);
-        } else {
-          setPublicaciones([]); // Si no se obtuvieron datos válidos, restablecer el estado a un arreglo vacío
-        }
-      })
-      .catch((error) => {
-        console.error('Error en la solicitud GET:', error);
-        setPublicaciones([]); // En caso de error, asegurarse de restablecer el estado a un arreglo vacío
-      });
-  };
 
   return (
     <View style={styles.container}>
@@ -42,9 +16,6 @@ export default function BusquedaScreen() {
       <ScrollView style={styles.scroll}>
         <View style={styles.contenedor1}>
           <Text style={styles.titulo}>Encuentra a tu mascota</Text>
-          <TouchableOpacity onPress={getPublicaciones}>
-            <Text>Obtener Publicaciones</Text>
-          </TouchableOpacity>
           <BarraBusqueda />
           <BotonesFiltros />
           <BotonFlotante />
